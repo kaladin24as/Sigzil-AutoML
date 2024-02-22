4d785c022083b03ddfbbca3fad3bc293b9bea038
kaladin24as <157048331+kaladin24as@users.noreply.github.com>
2024-02-22 00:04:34 -0300

    Delete Kaladin directory

--- Kaladin/copiar.py
+++
@@ -1,8 +0,0 @@
-# Tomas Farias
-# Modulo para copiar a portapapeles
-
-import pyperclip
-
-def copiar_a_portapeles(texto):
-    pyperclip.copy(texto)
-    print("Texto copiado al portapapeles:", texto)
